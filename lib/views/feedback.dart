37e94943ae6922499cb1bd7b860bae4e180762da
David Cruz <dcruzer92@gmail.com>
2024-11-02 23:01:50 -0600

    General: Ya se guardan comentarios y se muestra un toast cuando se realiza esa interacción

--- lib/views/feedback.dart
+++ lib/views/feedback.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:skytrack/utils/sidebar.dart';
@@ -10,6 +13,23 @@ class FeedbackPage extends StatefulWidget {
 }
 
 class _FeedbackPageState extends State<FeedbackPage> {
+  String selectedOption = '';
+  final TextEditingController _commentController = TextEditingController();
+  bool _isFirebaseInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeFirebase();
+  }
+
+  Future<void> initializeFirebase() async {
+    await Firebase.initializeApp();
+    setState(() {
+      _isFirebaseInitialized = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,39 +47,47 @@ class _FeedbackPageState extends State<FeedbackPage> {
         ),
       ),
       endDrawer: const Sidebar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Encuesta rápida',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                '¿Qué tan útil encuentras la app?',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 16),
-              _buildSurveyOptions(),
-              const SizedBox(height: 20),
-              const Text(
-                'Nuestros usuarios opinan',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              _buildPieChart(),
-              const SizedBox(height: 20),
-              const Text(
-                'Recomendaciones',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              _buildRecommendations(),
-            ],
-          ),
+      body: _isFirebaseInitialized
+          ? buildContent()
+          : const Center(
+              child:
+                  CircularProgressIndicator()), // Muestra un indicador de carga hasta que Firebase se inicialice
+    );
+  }
+
+  Widget buildContent() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Encuesta rápida',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              '¿Qué tan útil encuentras la app?',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
+            _buildSurveyOptions(),
+            const SizedBox(height: 20),
+            const Text(
+              'Nuestros usuarios opinan',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            _buildPieChart(),
+            const SizedBox(height: 20),
+            const Text(
+              'Recomendaciones',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            _buildRecommendations(),
+          ],
         ),
       ),
     );
@@ -71,30 +99,31 @@ class _FeedbackPageState extends State<FeedbackPage> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(child: _buildSurveyButton('Muy útil')),
+            Expanded(child: _buildSurveyButton('Muy útil', 1)),
             const SizedBox(width: 10),
-            Expanded(child: _buildSurveyButton('Útil')),
+            Expanded(child: _buildSurveyButton('Útil', 2)),
           ],
         ),
         const SizedBox(height: 10),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(child: _buildSurveyButton('Poco útil')),
+            Expanded(child: _buildSurveyButton('Poco útil', 3)),
             const SizedBox(width: 10),
-            Expanded(child: _buildSurveyButton('Nada útil')),
+            Expanded(child: _buildSurveyButton('Nada útil', 4)),
           ],
         ),
       ],
     );
   }
 
-  Widget _buildSurveyButton(String label) {
+  Widget _buildSurveyButton(String label, int value) {
     return SizedBox(
       height: 50,
       child: ElevatedButton(
         onPressed: () {
-          _showFeedbackModal(label);
+          selectedOption = label;
+          _showFeedbackModal(value);
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.lightBlueAccent,
@@ -107,7 +136,7 @@ class _FeedbackPageState extends State<FeedbackPage> {
     );
   }
 
-  void _showFeedbackModal(String option) {
+  void _showFeedbackModal(int utilidadValue) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -133,11 +162,12 @@ class _FeedbackPageState extends State<FeedbackPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                '¿Por qué nuestra app te parece $option?',
+                '¿Por qué nuestra app te parece $selectedOption?',
                 style: const TextStyle(fontSize: 14),
               ),
               const SizedBox(height: 10),
               TextField(
+                controller: _commentController,
                 maxLength: 150,
                 maxLines: 3,
                 decoration: InputDecoration(
@@ -151,8 +181,9 @@ class _FeedbackPageState extends State<FeedbackPage> {
           ),
           actions: [
             ElevatedButton(
-              onPressed: () {
-                // Acción al enviar el comentario
+              onPressed: () async {
+                await _saveFeedbackToFirestore(utilidadValue);
+                // ignore: use_build_context_synchronously
                 Navigator.of(context).pop();
               },
               style: ElevatedButton.styleFrom(
@@ -169,6 +200,43 @@ class _FeedbackPageState extends State<FeedbackPage> {
         );
       },
     );
+  }
+
+  Future<void> _saveFeedbackToFirestore(int utilidadValue) async {
+    if (!_isFirebaseInitialized) return;
+
+    final String comentario = _commentController.text;
+    final Map<String, dynamic> feedbackData = {
+      'id_usuario': 1,
+      'utilidad': utilidadValue,
+      'comentario': comentario,
+    };
+
+    try {
+      await FirebaseFirestore.instance
+          .collection('comentarios')
+          .add(feedbackData);
+      _commentController.clear();
+      // Show success toast
+      Fluttertoast.showToast(
+        msg: "Comentario añadido correctamente",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    } catch (e) {
+      // Show error toast
+      Fluttertoast.showToast(
+        msg: "Ha ocurrido un problema al guardar el comentario",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
   }
 
   Widget _buildPieChart() {
